5bd8d1bb1cde6c143d84d8abe0de29ce7a945e72
Phan Đức Huy <huyphanuyg@gmail.com>
2026-09-04 12:20:37 +0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Remove non-alphanumeric characters and convert to lowercase
+        cleaned_s = ''.join(char.lower() for char in s if char.isalnum())
+        # Check if the cleaned string is equal to its reverse
+        return cleaned_s == cleaned_s[::-1]
+
+        
