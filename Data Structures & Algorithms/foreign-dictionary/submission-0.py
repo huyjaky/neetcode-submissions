86e12dfcd70a349f069adb23496afc62f8ea05a2
Phan Đức Huy <huyphanuyg@gmail.com>
2026-09-16 11:39:35 +0700

    Add: foreign-dictionary - submission-0

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-0.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adj = {c: set() for word in words for c in word}
+        indegree = {c: 0 for c in adj}
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            length = min(len(w1), len(w2))
+
+            if len(w1) > len(w2) and w1[:length] == w2[:length]:
+                return ""
+
+            for j in range(length):
+                if w1[j] != w2[j]:
+                    if w2[j] not in adj[w1[j]]:
+                        adj[w1[j]].add(w2[j])
+                        indegree[w2[j]] += 1
+                    break
+
+        queue = [c for c in indegree if indegree[c] == 0]
+        res = []
+
+        while queue:
+            c = queue.pop()
+            res.append(c)
+
+            for nei in adj[c]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+
+        if len(res) != len(indegree):
+            return ""
+
+        return "".join(res)
